c4264bbfa1f74da160cf01341cbcd0ea0162e80e
= <riaanjlagrange@gmail.com>
2024-09-25 20:52:59 +0200

    fix typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,3 @@
-import Header from './components/Header'
-import Feature from './components/Feature'
-import News from './components/News'
-import Announcments from './components/Announcements'
-import Footer from './components/Footer'
 import Newsletter from './components/Newsletter'
 
 function App() {
@@ -12,11 +7,5 @@ function App() {
     </>
   );
 }
-
-      // <Header />
-      // <News />
-      // <Feature />
-      // <Announcments />
-      // <Footer />
 
 export default App
