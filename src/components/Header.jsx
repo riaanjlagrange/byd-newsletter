a721f31ee1a7c63fb74e38a524471c132580ae60
= <riaanjlagrange@gmail.com>
2024-09-25 18:53:18 +0200

    add footer and create file for components

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+  return (
+    <div className="flex w-[100vw] justify-center items-center gap-5 h-36 border-b-2 border-gray-500">
+      <img src="/logo.png" alt="logo" className="w-16 h-16" />         
+      <span className="text-xl font-bold">BYD Cafe</span>
+    </div>
+  );
+}
+
+
+export default Header;
